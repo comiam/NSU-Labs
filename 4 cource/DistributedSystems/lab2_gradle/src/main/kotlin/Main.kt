359610aa12038b59b216563c039efce6e967d36a
comiam <maxim.bolshim@yandex.ru>
2022-08-19 12:37:17 +0700

    fix some code in ds add evm labs

--- 4 cource/DistributedSystems/lab2_gradle/src/main/kotlin/Main.kt
+++ 4 cource/DistributedSystems/lab2_gradle/src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ import db.datasource.HikariConnectionManager
 import db.datasource.SingleConnectionManager
 
 class ResourceLoader {
-    val path: String? = this.javaClass.classLoader?.getResource("RU-NVS.osm.bz2")?.path
+    val path: String? = "/home/maxim/git/NSU-Labs/4 cource/DistributedSystems/lab2_gradle/build/resources/main/RU-NVS.osm.bz2"
 }
 
 // docker run --name postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=Osm -e POSTGRES_USER=postgres -e POSTGRES_INITDB_ARGS="-E UTF8" -d -p 5432:5432 postgres
