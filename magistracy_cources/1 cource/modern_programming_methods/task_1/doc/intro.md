9ca6857c33cd410e3103ee03fd8766c87ca95d7b
Maxim Maximov <maxim.bolshim@yandex.ru>
2022-09-30 11:20:28 +0700

    Add magistracy cources

---
+++ magistracy_cources/1 cource/modern_programming_methods/task_1/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to task_1
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ magistracy_cources/1 cource/modern_programming_methods/task_1/src/task_1/task_1_2.clj
@@ -0,0 +1,39 @@
+(ns task-1.task_1_2)
+
+(defn append_char_to_word
+  "Соединяет символ со словом, если такой же символ не встречается в начале слова"
+  [word char]
+  (if (= (first word) char)
+    word
+    (str char word)))
+
+(defn concat_seq_to_word
+  "Делает подпоследовательность слов, соединяя текущее слово со всеми символами из набора"
+  [word seq seq_i words_acc]
+  (if (= seq_i (count seq))                                 ; если соединили уже все символы со словом
+    words_acc
+    (let [new_word (append_char_to_word word (nth seq seq_i))
+          new_seq (inc seq_i)]
+      (if (= word new_word)                                 ; если случился повтор подряд идущих символов
+        (recur word seq new_seq words_acc)
+        (recur word seq new_seq (doall (concat words_acc (list new_word))))))))
+
+(defn concat_seq_to_words
+  "Делает новые подпоследовательности слов, добавляя символы"
+  [words seq words_acc words_i]
+  (if (= (count words) words_i)                             ; если перебрали все слова
+    words_acc
+    (recur words seq (doall (concat words_acc (concat_seq_to_word (nth words words_i) seq 0 []))) (inc words_i))))
+
+(defn seq_all_words
+  "Создаёт последовательность секвенций символов char_seq длины n без повторяющихся попарно символов"
+  [words_acc seq n]
+  (if (= 0 (count seq))                                     ; проверка на пустой набор
+    []
+    (if (= 0 (count words_acc))                             ; начало рекурсии
+      (recur (map str seq) seq n)
+      (if (= (count (nth words_acc 0)) n)                   ; если длина слов равна n
+        words_acc
+        (recur (concat_seq_to_words words_acc seq [] 0) seq n)))))
+
+(println (seq_all_words [] `(\a \b \c) 3))
